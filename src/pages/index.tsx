0c77bcf71a71a0a1359232502294584ebe0400ba
mikana0918 <shogo.kusuhara@gmail.com>
2023-06-01 03:11:40 +0900

    feat: Add business

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import DefaultLayout from "@/layouts/DefaultLayout";
 import CallToActionWithAnnotation from "@/components/Hero/CTAWithAnnotation";
 import About from "@/components/Features/About";
+import Businesses from "@/components/Features/Businesses";
+import { Spacer } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <DefaultLayout>
       <CallToActionWithAnnotation />
       <About />
+      <Spacer mt={12} />
+      <Businesses />
     </DefaultLayout>
   );
 }
