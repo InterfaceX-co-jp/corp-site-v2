c746228eb56e2a6aca4b01dd2e3de5198c876a45
mikana0918 <shogo.kusuhara@gmail.com>
2023-06-01 02:21:47 +0900

    feat: Layout

--- src/components/Hero/CTAWithAnnotation.tsx
+++ src/components/Hero/CTAWithAnnotation.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 import {
   Box,
   Heading,
@@ -9,69 +9,70 @@ import {
   Icon,
   useColorModeValue,
   createIcon,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
 export default function CallToActionWithAnnotation() {
   return (
     <>
-      <Container maxW={'3xl'}>
+      <Container maxW={"3xl"}>
         <Stack
           as={Box}
-          textAlign={'center'}
+          textAlign={"center"}
           spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}>
+          py={{ base: 20, md: 36 }}
+        >
           <Heading
             fontWeight={600}
-            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}>
-            Make money from <br />
-            <Text as={'span'} color={'green.400'}>
-              your audience
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            Develop The Best <br />
+            <Text as={"span"} color={"green.400"}>
+              最高のプロダクトを作る
             </Text>
           </Heading>
-          <Text color={'gray.500'}>
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
-          </Text>
+          <Text color={"gray.500"}>ああああ最高のプロダクト開発組織を作る</Text>
           <Stack
-            direction={'column'}
+            direction={"column"}
             spacing={3}
-            align={'center'}
-            alignSelf={'center'}
-            position={'relative'}>
+            align={"center"}
+            alignSelf={"center"}
+            position={"relative"}
+          >
             <Button
-              colorScheme={'green'}
-              bg={'green.400'}
-              rounded={'full'}
+              colorScheme={"green"}
+              bg={"green.400"}
+              rounded={"full"}
               px={6}
               _hover={{
-                bg: 'green.500',
-              }}>
+                bg: "green.500",
+              }}
+            >
               Get Started
             </Button>
-            <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+            <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
               Learn more
             </Button>
-            <Box>
+            {/* <Box>
               <Icon
                 as={Arrow}
-                color={useColorModeValue('gray.800', 'gray.300')}
+                color={useColorModeValue("gray.800", "gray.300")}
                 w={71}
-                position={'absolute'}
+                position={"absolute"}
                 right={-71}
-                top={'10px'}
+                top={"10px"}
               />
               <Text
-                fontSize={'lg'}
-                fontFamily={'Caveat'}
-                position={'absolute'}
-                right={'-125px'}
-                top={'-15px'}
-                transform={'rotate(10deg)'}>
+                fontSize={"lg"}
+                fontFamily={"Caveat"}
+                position={"absolute"}
+                right={"-125px"}
+                top={"-15px"}
+                transform={"rotate(10deg)"}
+              >
                 Starting at $15/mo
               </Text>
-            </Box>
+            </Box> */}
           </Stack>
         </Stack>
       </Container>
@@ -80,8 +81,8 @@ export default function CallToActionWithAnnotation() {
 }
 
 const Arrow = createIcon({
-  displayName: 'Arrow',
-  viewBox: '0 0 72 24',
+  displayName: "Arrow",
+  viewBox: "0 0 72 24",
   path: (
     <path
       fillRule="evenodd"
