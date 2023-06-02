e30681efa49116cdb67df2eeff920f832f93a520
mikana0918 <shogo.kusuhara@gmail.com>
2023-06-03 01:37:07 +0900

    feat: meta

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -5,8 +5,14 @@ export const pagesPath = {
 export type PagesPath = typeof pagesPath
 
 export const staticPath = {
+  android_chrome_192x192_png: '/android-chrome-192x192.png',
+  android_chrome_512x512_png: '/android-chrome-512x512.png',
+  apple_touch_icon_png: '/apple-touch-icon.png',
+  favicon_16x16_png: '/favicon-16x16.png',
+  favicon_32x32_png: '/favicon-32x32.png',
   favicon_ico: '/favicon.ico',
   next_svg: '/next.svg',
+  site_webmanifest: '/site.webmanifest',
   vercel_svg: '/vercel.svg'
 } as const
 
